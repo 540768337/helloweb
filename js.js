5a106fbd9152d4827e4f757eda9631e5a116f5a1
540768337 <540768337@qq.com>
2018-05-13 09:02:23 +0800

    tmall

---
+++ js.js
@@ -0,0 +1,44 @@
+/**
+ * Created by ts on 2018/5/9.
+ */
+编程语言：c,c++,java.不需借助其他东西可以自己运行
+脚本语言
+
+
+
+
+1.html
+2.css
+3.js
+    3.1客户端js
+    3.2服务端js：node.js
+4.php (python) & node.js
+
+《javascipt权威指南第六版》
+《javascipt高级程序设计第3版》
+
+区块链
+
+习惯写在body最后面
+js是弱类型的语言
+js数据类型：
+一、基本数据类型：number、string、boolean、undefined、null
+二、引用类型：对象（object）
+
+对象：everything is object 一切皆对象
+
+1.内置对象
+2.dom对象（前端中特有的概念）
+3.自定义对象
+
+switch 只能选择固定值，不会自动进行数据类型转换
+if可以选择区间
+
+避免使用全局变量
+var aa = function（函数表达式）
+function () {
+                （函数声明）
+}
+push：往数组的末尾插入
+unshift：往数组前面投东西
+把数组转化成字符串
